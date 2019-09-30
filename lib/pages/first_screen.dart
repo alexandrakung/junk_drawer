f0762aa2595fe4f67a5e1c32729df4a5d3002109
Alexandra Kung <alexandrakung@Alexandras-MacBook-Pro.local>
2019-09-30 15:56:07 -0500

    search bar

--- lib/pages/first_screen.dart
+++ lib/pages/first_screen.dart
@@ -1,10 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:junk_drawer/sign_in.dart';
 
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.blue[100]),
-    );
+        appBar: PreferredSize(
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.0, left: 10.0, right: 10.0),
+            child: Card(
+              elevation: 6.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5.0),
+                  ),
+                ),
+                child: TextField(
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    hintText: "Search...",
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.filter_list,
+                      color: Colors.black,
+                    ),
+                    hintStyle: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                  maxLines: 1,
+                ),
+              ),
+            ),
+          ),
+          preferredSize: Size(
+            MediaQuery.of(context).size.width,
+            60.0,
+          ),
+        ),
+        body: Center(
+            child: PreferredSize(
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.0, left: 10.0, right: 10.0),
+            child: Card(
+              elevation: 6.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          preferredSize: Size(
+            MediaQuery.of(context).size.width,
+            60.0,
+          ),
+        )),
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.menu_close,
+          children: [
+            SpeedDialChild(
+                child: Icon(Icons.folder),
+                label: "Add item",
+                onTap: () => print("first")),
+            SpeedDialChild(
+                child: Icon(Icons.ac_unit),
+                label: "Add drawer",
+                onTap: () => print("first"))
+          ],
+        ));
   }
 }
